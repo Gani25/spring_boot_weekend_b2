8dadf2bfdfc3280d20647d9e2b964ac223dcc9bd
Gani25 <memonabdulgani3d@gmail.com>
2025-08-23 18:08:10 +0530

    Pathvariable and request params

--- springboot_demo/src/main/java/com/sprk/springboot_demo/controller/DemoController.java
+++ springboot_demo/src/main/java/com/sprk/springboot_demo/controller/DemoController.java
@@ -1,7 +1,6 @@
 package com.sprk.springboot_demo.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -11,7 +10,8 @@ import java.util.List;
 public class DemoController {
 
     // Methods
-    @RequestMapping("/hello")
+//    @RequestMapping(value = {"/hello","/home"}, method = RequestMethod.GET)
+    @GetMapping({"/hello","/home"})
     public String sayHello(){
         return "Hello World";
     }
@@ -30,4 +30,24 @@ public class DemoController {
     public int sayNumber(){
         return 50;
     }
+
+    // Request Parameters
+    // create a mapping which accept 2 numbers display product of that number
+    @GetMapping("/api/req-param/add")
+    public String sayAddition(@RequestParam(name = "n1", defaultValue = "0") int num1,@RequestParam(defaultValue = "0") int num2){
+        int result = num1 + num2;
+        return String.format("The addition of %d, %d = %d",num1,num2,result);
+    }
+
+    // Path Variable
+    @GetMapping("/api/path-variable/add/{n1}/{num2}")
+    public String sayAdditionPathVariable(@PathVariable(name = "n1") int num1,@PathVariable int num2){
+        int result = num1 + num2;
+        return String.format("The addition of %d, %d = %d with path variable",num1,num2,result);
+    }
+
+    // create a mapping which accepts a number display fibonacci series till that number
+    // input = 8 ->
+    // Fibonacci Series till 8 iterations are:
+    // 0 1 2 3 5 8 13 21
 }
